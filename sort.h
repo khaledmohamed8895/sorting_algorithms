c4ca15a43fbebe4409a5280b11fb34a11d3c2538
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-01-27 03:07:13 +0200

    finished task 103-merge_sortin

--- sort.h
+++ sort.h
@@ -44,4 +44,5 @@ void quicksort_recusion(int array[], int lower, int upper, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
 #endif /* SORT_H */

83c40ad69d0704c6a5e571cf75b0d296726263b7
khaledmohamed8895 <khaledmohamed8995@gmail.com>
2024-01-27 03:00:46 +0200

    finished task 101-cocktail_sort_list.c

--- sort.h
+++ sort.h
@@ -41,7 +41,7 @@ void quick_sort(int *array, size_t size);
 void swap(int *x, int *y);
 int partition(int array[], int lower, int upper, size_t size);
 void quicksort_recusion(int array[], int lower, int upper, size_t size);
-
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 #endif /* SORT_H */
